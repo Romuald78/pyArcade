09768e2b73acd30c3de01874d743d5e07d96857e
Pierre-Henri <pierreh37@gmail.com>
2020-03-06 15:25:48 +0100

    Final Release for students

--- venv/Launcher.py
+++ venv/Launcher.py
@@ -4,15 +4,14 @@
 import arcade
 import random
 import time
-
+import StudentCode
+from Utils import *
 
 
 ### ====================================================================================================
 ### CONSTANTS
 ### ====================================================================================================
-SCREEN_WIDTH = 960
-SCREEN_HEIGHT = 540
-TITLE = "My supa game in Python !"
+TITLE = "Coding Factory Mini-Game !"
 
 
 
@@ -74,60 +73,7 @@ class MyGame(arcade.Window):
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     def setup(self):
         #- - - - - - - - - - - - - - - - - - - - - - - - -#
-        self.circleX = SCREEN_WIDTH  / 2
-        self.circleY = SCREEN_HEIGHT / 2
-        self.radius  = 75
-        self.move = 0
-
-        #init SPRITES
-        self.robot = {"idle":None, "walk":None}
-
-        # Add IDLE
-        filepath = f":resources:images/animated_characters/robot/robot_idle.png"
-        self.robot["idle"] = arcade.AnimatedTimeSprite()
-        self.robot["idle"].textures = []
-        self.robot["idle"].textures.append(arcade.load_texture(filepath))
-
-        # Add WALK
-        self.robot["walk"] = {"left":arcade.AnimatedTimeSprite(),"right":arcade.AnimatedTimeSprite()}
-        self.robot["walk"]["left"].textures  = []
-        self.robot["walk"]["right"].textures = []
-        for i in range(8):
-            filepath = f":resources:images/animated_characters/robot/robot_walk{i}.png"
-            self.robot["walk"]["left"].textures.append(  arcade.load_texture(filepath, mirrored=True) )
-            self.robot["walk"]["right"].textures.append( arcade.load_texture(filepath) )
-
-        # select current sprite
-        self.currentSprite = self.robot["idle"]
-
-        # Load box sprites
-        bSize = 75
-        self.boxes = []
-        for i in range(10):
-            xb = random.random() * SCREEN_WIDTH
-            yb = random.random() * SCREEN_HEIGHT
-            self.boxes.append( arcade.AnimatedTimeSprite() )
-            self.boxes[i].textures = []
-            filepath = f":resources:images/tiles/boxCrate_double.png"
-            self.boxes[i].textures.append( arcade.load_texture(filepath) )
-            self.boxes[i].set_position(int(xb),int(yb))
-            self.boxes[i].set_points([[-bSize,-bSize],[bSize,-bSize],[bSize,bSize],[-bSize,bSize]])
-            self.boxes[i].update_animation(0)
-            self.boxes[i].scale = 0.5
-
-        self.emit = arcade.Emitter(
-            center_xy        = (SCREEN_HEIGHT/2, SCREEN_WIDTH/2),
-            emit_controller  = arcade.EmitMaintainCount(400),
-            particle_factory = lambda emitter: arcade.FadeParticle(
-                filename_or_texture = arcade.make_circle_texture(30, arcade.color.WHITE),
-                change_xy           = arcade.rand_in_circle((0.0, 0.0), random.uniform(0.5,0.6)),
-                lifetime            = random.uniform(0.1, 5.0),
-                scale = 0.1,
-                start_alpha=100,
-                end_alpha=0,
-            ),
-        )
-
+        StudentCode.setup()
         #- - - - - - - - - - - - - - - - - - - - - - - - -#
 
 
@@ -137,17 +83,7 @@ class MyGame(arcade.Window):
     def on_draw(self):
         #- - - - - - - - - - - - - - - - - - - - - - - - -#
         arcade.start_render()
-
-        # draw boxes
-        for b in self.boxes:
-            b.draw()
-
-        # draw robot
-        self.currentSprite.draw()
-
-
-
-        self.emit.draw()
+        StudentCode.draw()
         #- - - - - - - - - - - - - - - - - - - - - - - - -#
 
 
@@ -156,75 +92,7 @@ class MyGame(arcade.Window):
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     def update(self, delta_time):
         #- - - - - - - - - - - - - - - - - - - - - - - - -#
-        # update position and radius according to move variable
-        movX = 0
-        movY = 0
-        if self.move & 1 != 0:
-            movY += 5
-        if self.move & 2 != 0:
-            movY -= 5
-        if self.move & 4 != 0:
-            movX -= 5
-        if self.move & 8 != 0:
-            movX += 5
-
-        # Check if robot is in collision with any box
-        curX = self.circleX
-        curY = self.circleY
-        nxtX = self.circleX + movX
-        nxtY = self.circleY + movY
-        for b in self.boxes:
-            pts = b.get_points()
-            minX = pts[0][0]
-            maxX = pts[2][0]
-            minY = pts[0][1]
-            maxY = pts[2][1]
-            if nxtX >= minX and nxtX <= maxX and nxtY >= minY and nxtY <= maxY :
-                # check if we push HORIZONTALLY
-                if curY >= minY and curY <=maxY:
-                    if curX < minX:
-                        b.center_x = b.center_x + 5
-                    elif curX > maxX:
-                        b.center_x = b.center_x - 5
-                # push VERTICALLY
-                if curX >= minX and curX <=maxX:
-                    if curY < minY:
-                        b.center_y = b.center_y + 5
-                    elif curY > maxY:
-                        b.center_y = b.center_y - 5
-
-        # update position
-        self.circleX += movX
-        self.circleY += movY
-
-        # block robot position to the screen borders
-        if self.circleX < 0:
-            self.circleX = 0;
-        if self.circleX > SCREEN_WIDTH:
-            self.circleX = SCREEN_WIDTH;
-        if self.circleY < 0:
-            self.circleY = 0;
-        if self.circleY > SCREEN_HEIGHT:
-            self.circleY = SCREEN_HEIGHT;
-
-        # Select robot animation
-        self.currentSprite = self.robot["idle"]
-        if self.move != 0:
-            i = int(time.time() * 1000 / 100) % 8;
-            dir = "right"
-            if (self.move & 4) == 4:
-                dir = "left"
-            self.currentSprite = self.robot["walk"][dir]
-
-        # update animation and position of the robot
-        self.currentSprite.update_animation(delta_time)
-        self.currentSprite.set_position(self.circleX,self.circleY+60)
-
-
-        self.emit.center_x = self.circleX
-        self.emit.center_y = self.circleY + self.currentSprite.height*0.5
-        self.emit.update()
-
+        StudentCode.update(delta_time)
         #- - - - - - - - - - - - - - - - - - - - - - - - -#
 
 
@@ -236,16 +104,7 @@ class MyGame(arcade.Window):
         # Close application if ESCAPE key is hit
         if key == arcade.key.ESCAPE:
             self.close()
-
-        # Store movement according to key press
-        if key == arcade.key.UP:
-            self.move |= 1
-        if key == arcade.key.DOWN:
-            self.move |= 2
-        if key == arcade.key.LEFT:
-            self.move |= 4
-        if key == arcade.key.RIGHT:
-            self.move |= 8
+        StudentCode.onKeyEvent(key,True)
         #- - - - - - - - - - - - - - - - - - - - - - - - -#
 
 
@@ -254,15 +113,7 @@ class MyGame(arcade.Window):
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     def on_key_release(self, key, modifiers):
         #- - - - - - - - - - - - - - - - - - - - - - - - -#
-        # Store movement according to key release
-        if key == arcade.key.UP:
-            self.move &=  ~1
-        if key == arcade.key.DOWN:
-            self.move &=  ~2
-        if key == arcade.key.LEFT:
-            self.move &=  ~4
-        if key == arcade.key.RIGHT:
-            self.move &=  ~8
+        StudentCode.onKeyEvent(key,False)
         #- - - - - - - - - - - - - - - - - - - - - - - - -#
 
 
@@ -298,20 +149,7 @@ class MyGame(arcade.Window):
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     def onAxisMove(self, gamepadNum, axisName, analogValue):
         #- - - - - - - - - - - - - - - - - - - - - - - - -#
-        if axisName == 'x':
-            if analogValue >= 0.5:
-                self.move |= 8
-            elif analogValue <= -0.5:
-                self.move |= 4
-            else:
-                self.move &= ~12
-        if axisName == 'y':
-            if analogValue >= 0.5:
-                self.move |= 2
-            elif analogValue <= -0.5:
-                self.move |= 1
-            else:
-                self.move &= ~3
+        pass
         #- - - - - - - - - - - - - - - - - - - - - - - - -#
 
 
--- venv/StudentCode.py
+++ venv/StudentCode.py
@@ -9,110 +9,42 @@ var = Variables()
 
 
 
-
-
-
+### ====================================================================================================
+### INITIALISATION OF YOUR VARIABLES
+### ====================================================================================================
 def setup():
-    print("setup")
-
-    # BACKGROUND
-    arcade.set_background_color(arcade.color.BLUE)
-    var.bg = createSprite(f":resources:images/backgrounds/abstract_1.jpg",(SCREEN_WIDTH,SCREEN_HEIGHT),True)
-    var.bg.center_x = SCREEN_WIDTH / 2
-    var.bg.center_y = SCREEN_HEIGHT / 2
-
     # ROBOT SPRITE
     var.robot = createSprite(f":resources:images/animated_characters/robot/robot_idle.png",(200,200))
-
     # robot position
     var.robot.center_x = 100
     var.robot.center_y = 100
 
-    # movement
-    var.moveLeft  = False
-    var.moveRight = False
-
-    # ONE FRUIT
-    var.fruits = []
 
 
-    var.life = 3
-    var.stars = []
-    for i in range(var.life):
-        star = createSprite(f":resources:images/items/star.png",(100,100))
-        star.center_y = SCREEN_HEIGHT - 50;
-        star.center_x = 50 + 75 * i
-        var.stars.append(star)
-
-    var.time = 0
-
-    var.score = 0
-
-
-
+### ====================================================================================================
+### UPDATE OF YOUR GAME DATA
+### ====================================================================================================
 def update(deltaTime):
-    var.time += deltaTime
-
-    if var.life > 0:
-
-        if var.moveLeft:
-            var.robot.center_x -= 7;
-        if var.moveRight:
-            var.robot.center_x += 7;
-
-        if var.robot.center_x < 0:
-            var.robot.center_x = 0
-
-        if var.robot.center_x > SCREEN_WIDTH:
-            var.robot.center_x = SCREEN_WIDTH
+   pass
 
 
 
-        if var.time >= 1:
-            var.time -= 1
-            fruit = createSprite(f":resources:images/items/gemYellow.png",(75,75));
-            fruit.center_x = randint(0,SCREEN_WIDTH)
-            fruit.center_y = SCREEN_HEIGHT+100
-            var.fruits.append( fruit )
-
-
-
-
-
-    for f in var.fruits:
-        f.center_y -= 5
-
-        if f.center_y < 100:
-            if abs( var.robot.center_x - f.center_x) < 50:
-                var.fruits.remove(f)
-                var.score += 1
-
-        if f.center_y < 0:
-            var.fruits.remove(f)
-            var.life -= 1
-
-
+### ====================================================================================================
+### DRAW YOUR IMAGES ON THE SCREEN
+### ====================================================================================================
 def draw():
-    var.bg.draw()
-
     var.robot.draw()
 
-    for i in range(var.life):
-        var.stars[i].draw()
-
-    arcade.draw_text("SCORE = "+str(var.score), SCREEN_WIDTH-25, SCREEN_HEIGHT-45, arcade.color.ORANGE, 30, anchor_x="right", anchor_y="top", bold=True    )
-
-    for f in var.fruits:
-        f.draw()
 
 
+### ====================================================================================================
+### FUNCTION CALLED WHEN YOU PRESS A KEY ON THE KEYBOARD
+### ====================================================================================================
 def onKeyEvent(key,isPressed):
-    if key == arcade.key.LEFT:
-        var.moveLeft = isPressed
-    if key == arcade.key.RIGHT:
-        var.moveRight = isPressed
+    if key == arcade.key.SPACE:
+        if isPressed :
+            print("SPACE KEY has been pressed !")
+        else:
+            print("SPACE KEY has been released !")
 
-    if key == arcade.key.ENTER:
-        var.life = 3
-        var.fruits = []
-        var.score = 0
+
