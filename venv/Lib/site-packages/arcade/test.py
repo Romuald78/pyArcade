c48e9a314b3833bfe0afd1c5662696db3511fdb4
Romuald GRIGNON <romuald.grignon@gmail.com>
2020-03-06 12:15:07 +0100

    update venv

---
+++ venv/Lib/site-packages/arcade/test.py
@@ -0,0 +1,134 @@
+"""
+Move Sprite With Keyboard
+
+Simple program to show moving a sprite with the keyboard.
+The sprite_move_keyboard_better.py example is slightly better
+in how it works, but also slightly more complex.
+
+Artwork from http://kenney.nl
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.sprite_move_keyboard
+"""
+
+import arcade
+import os
+
+SPRITE_SCALING = 0.5
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Move Sprite with Keyboard Example"
+
+MOVEMENT_SPEED = 5
+
+
+class Player(arcade.Sprite):
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self, width, height, title):
+        """
+        Initializer
+        """
+
+        # Call the parent class initializer
+        super().__init__(width, height, title)
+
+        # Variables that will hold sprite lists
+        self.player_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player
+        self.player_sprite = Player(":resources:images/animated_characters/female_person/femalePerson_idle.png", SPRITE_SCALING)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw all the sprites.
+        self.player_list.draw()
+        self.player_sprite.draw_hit_box()
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Call update on all sprites (The sprites don't do much in this
+        # example though.)
+        self.player_list.update()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.A:
+            print(f"Before {self.player_sprite.scale} {self.player_sprite.left}")
+            self.player_sprite.scale = 2
+            print(f"After {self.player_sprite.scale} {self.player_sprite.left}")
+        elif key == arcade.key.B:
+            print(f"Before {self.player_sprite.scale} {self.player_sprite.left}")
+            self.player_sprite.scale = 1
+            print(f"After {self.player_sprite.scale} {self.player_sprite.left}")
+        elif key == arcade.key.Z:
+            self.player_sprite.center_x = 0
+            self.player_sprite.center_y = 0
+        elif key == arcade.key.X:
+            self.player_sprite.left = 0
+        elif key == arcade.key.C:
+            self.player_sprite.width = 100
+            print(f"Before {self.player_sprite.scale} {self.player_sprite.left}")
+            self.player_sprite.width = 200
+            print(f"After {self.player_sprite.scale} {self.player_sprite.left}")
+
+
+def main():
+    """ Main method """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
